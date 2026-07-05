5d48404ec73bf1e78ab7ec79aaf475e99edd6b1c
John Greening <johngreening@gmail.com>
2026-07-05 11:19:40 +0100

    Add files via upload
    wrapped player and skull wall collision checks into consolidated checkupM/checkDownM/checkLeftM/checkRightM macros.
    significant performace fixes to these so that we don't page in data everytime if on same page.

--- maze.asm
+++ maze.asm
@@ -20,6 +20,7 @@ CAM_MAX_Y           equ (192 - 32) * 8              ; 1280  (map height - visibl
 
 
 tilemapPage         db 0
+curWorldPage        db 0
 
 playerHealth        db 8
 healthBlock         db 23
@@ -459,104 +460,25 @@ doMoveDone:
         jp moveSprite
         
 chkLeft:
-	    ld hl, (player_px)                          ; get player px
-	    ld bc, move_delta                           ; get move delta
-	    sbc hl, bc                                  ; "try" move
-	
-	    ; now test if we have bumped into a wall
-	    DIV_HL_8
-	    ld b, l
-	    ld hl, (player_py)
-	    DIV_HL_8
-	    ld c, l
-	    GET_WORLD_VALUE TILEMAP_PAGE, USE_BC
-	    cp MAP_WALL
-	    ret z
-	    inc c
-	    GET_WORLD_VALUE TILEMAP_PAGE, USE_BC
-	    cp MAP_WALL
-	    ret z
-	
-	    set dir_left, e                             ; set ok
-        ret                                         ; return
+        checkLeftM player_px, player_py, move_delta
 
 chkRight:
-	    ld hl, (player_px)                          ; get player px
-	    ld bc, move_delta                           ; get move delta
-        adc hl, bc
-
-        ; now test if we have bumped into a wall
-        add hl, 15                                  ; the sprite extends 0-15px right
-        DIV_HL_8
-        ld b, l
-        ld hl, (player_py)
-        DIV_HL_8
-        ld c, l
-        GET_WORLD_VALUE TILEMAP_PAGE, USE_BC
-        cp MAP_WALL
-        ret z
-        inc c
-        GET_WORLD_VALUE TILEMAP_PAGE, USE_BC
-        cp MAP_WALL
-        ret z
-
-        set dir_right, e
-        ret
+        checkRightM player_px, player_py, move_delta
 
 chkDown:
-        ld hl, (player_py)
-        ld bc, move_delta                           ; get pixel delta
-        adc hl, bc                                  ; do move
-
-        ; now test if we have bumped into a wall
-        add hl, 15                                  ; the sprite extends 0-15px down
-        DIV_HL_8
-        ld c, l
-        ld hl, (player_px)
-        DIV_HL_8
-        ld b, l
-        GET_WORLD_VALUE TILEMAP_PAGE, USE_BC
-        cp MAP_WALL
-        ret z
-        inc b
-        GET_WORLD_VALUE TILEMAP_PAGE, USE_BC
-        cp MAP_WALL
-        ret z
-
-        set dir_down, e
-        ret
+        checkDownM player_px, player_py, move_delta
 
 chkUp:
-        ld hl, (player_py)                          ; get player y
-        ld bc, move_delta                           ; get pixel delta
-        sbc hl, bc                                  ; do move
-
-        ; now test if we have bumped into a wall
-        DIV_HL_8
-        ld c, l
-        ld hl, (player_px)
-        DIV_HL_8
-        ld b, l
-        GET_WORLD_VALUE TILEMAP_PAGE, USE_BC
-        cp MAP_WALL
-        ret z
-        inc b
-        GET_WORLD_VALUE TILEMAP_PAGE, USE_BC
-        cp MAP_WALL
-        ret z
-
-        set dir_up, e
-        ret
-
+        checkUpM player_px, player_py, move_delta
 
 keyEnd:
 moveSprite:
 ;        call debugs
-        ;ld a, 2
-        ;out (254), a
+;        ld a, 2
+;        out (254), a
         call processBaddies
-        ;xor a
-        ;out (254), a
+;        xor a
+;        out (254), a
 
 moveSprite1:
 ; ================================================
@@ -1133,108 +1055,28 @@ chkLeft1
         and a                                       ; is it?
         ret nz                                      ; return if not
 
-        ld hl, (baddie_x)                           ; get current baddie X pixel
-        ld bc, move_delta_baddie                    ; x pixel delta
-        sbc hl, bc                                  ; simulate move left
-
-        ; now test if we have bumped into a wall
-        DIV_HL_8                                    ; divide by 8 to get tile X
-        ld b, l                                     ; B = tile X
-        ld hl, (baddie_y)                           ; get current baddie Y pixel
-        DIV_HL_8                                    ; divide by 8 to get tile Y
-        ld c, l                                     ; C = tile Y
-        GET_WORLD_VALUE TILEMAP_PAGE, USE_BC        ; see what tile is here
-        cp MAP_WALL                                 ; is it a wall?
-        ret z                                       ; return if so, do nothing
-
-        inc c                                       ; sprites are 2 tiles tall, so we will check tile Y+1
-        GET_WORLD_VALUE TILEMAP_PAGE, USE_BC        ; see what tile is here
-        cp MAP_WALL                                 ; is it a wall?
-        ret z                                       ; return if so, do nothing
-
-        set dir_left, e                             ; set bit in E indicating move is allowed
-        ret                                         ; and return
+        checkLeftM baddie_x, baddie_y, move_delta_baddie
 
 chkRight1
         ld a, iyl                                   ; we only want to test horizontal moves if baddie Y is exactly on a tile
         and a                                       ; is it?
         ret nz                                      ; return if not
 
-        ld hl, (baddie_x)
-	    ld bc, move_delta_baddie                    ; get move delta
-        adc hl, bc
-
-        ; now test if we have bumped into a wall
-        add hl, 15                                  ; the sprite extends 0-15px right
-        DIV_HL_8
-        ld b, l
-        ld hl, (baddie_y)
-        DIV_HL_8
-        ld c, l
-        GET_WORLD_VALUE TILEMAP_PAGE, USE_BC
-        cp MAP_WALL
-        ret z
-        inc c
-        GET_WORLD_VALUE TILEMAP_PAGE, USE_BC
-        cp MAP_WALL
-        ret z
-
-        set dir_right, e
-        ret
+        checkRightM baddie_x, baddie_y, move_delta_baddie
 
 chkDown1
         ld a, iyh                                   ; we only want to test vertical moves if baddie X is exactly on a tile
         and a                                       ; is it?
         ret nz                                      ; return if not
 
-        ld hl, (baddie_y)
-        ld bc, move_delta_baddie                    ; get pixel delta
-        adc hl, bc                                  ; do move
-
-        ; now test if we have bumped into a wall
-        add hl, 15                                  ; the sprite extends 0-15px down
-        DIV_HL_8
-        ld c, l
-        ld hl, (baddie_x)
-        DIV_HL_8
-        ld b, l
-        GET_WORLD_VALUE TILEMAP_PAGE, USE_BC
-        cp MAP_WALL
-        ret z
-        inc b
-        GET_WORLD_VALUE TILEMAP_PAGE, USE_BC
-        cp MAP_WALL
-        ret z
-
-        set dir_down, e
-        ret
+        checkDownM baddie_x, baddie_y, move_delta_baddie
 
 chkUp1
         ld a, iyh                                   ; we only want to test vertical moves if baddie X is exactly on a tile
         and a                                       ; is it?
         ret nz                                      ; return if not
 
-        ld hl, (baddie_y)
-        ld bc, move_delta_baddie                    ; get pixel delta
-        sbc hl, bc                                  ; do move
-
-        ; now test if we have bumped into a wall
-        DIV_HL_8
-        ld c, l
-        ld hl, (baddie_x)
-        DIV_HL_8
-        ld b, l
-        GET_WORLD_VALUE TILEMAP_PAGE, USE_BC
-        cp MAP_WALL
-        ret z
-        inc b
-        GET_WORLD_VALUE TILEMAP_PAGE, USE_BC
-        cp MAP_WALL
-        ret z
-
-        set dir_up, e
-        ret
-
+        checkUpM baddie_x, baddie_y, move_delta_baddie
 
 doMoveLeft1
         ld hl, (baddie_x)
